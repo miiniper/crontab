66e556de6a6408f36a2772cf72f5829df11e0916
miiniper <34260823+miiniper@users.noreply.github.com>
2022-11-28 17:08:39 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -4,5 +4,7 @@ token=${WEIXIN_TOKEN}
 now=`date "+%F %H:%M:%S"`
 
 # test
+echo ==
 echo $token
+echo ==
 curl -X POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='$token --header 'Content-Type:application/json' --data '{"msgtype":"text","text":{"content":"生活小蜜提醒您现在是 '"$now"' ,您该喝水了，休息一下"}}'
